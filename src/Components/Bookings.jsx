34a6087eb303cd0bd76e987feea4e2778156d834
Arman Ali <aa278012@gmail.com>
2024-11-27 19:00:56 +0530

    bug fixed

--- src/Components/Bookings.jsx
+++ src/Components/Bookings.jsx
@@ -36,63 +36,63 @@ const Bookings = ({ history }) => {
     });
   };
 
+  console.log(history);
+
   return (
     <div>
-      <div>
-        <div className="sm:max-w-5xl w-full mx-auto sm:my-8 space-y-4">
-          <div className="top-detail-section">
-            <p className="text-gray-500 font-light">Your Previous Bookings</p>
-          </div>
-          {history?.data?.map((consultation, index) => (
-            <div key={index} className="bg-white shadow-lg rounded-lg">
-              <div
-                className="flex justify-between items-center p-4 cursor-pointer bg-[#1495AB] text-white rounded-t-lg"
-                onClick={() => toggleSection(index)}
-              >
-                <h3 className="text-sm font-bold text-white">
-                  Consultation Date:{" "}
-                  {convertToIST(consultation?.bookingDetails?.createdAt)}
-                </h3>
-                {openSection === index ? <FaChevronUp /> : <FaChevronDown />}
-              </div>
+      {history?.data?.length > 0 ? (
+        <div>
+          <div className="sm:max-w-5xl w-full mx-auto sm:my-8 space-y-4">
+            <div className="top-detail-section">
+              <p className="text-gray-500 font-light">Your Previous Bookings</p>
+            </div>
+            {history?.data?.map((consultation, index) => (
+              <div key={index} className="bg-white shadow-lg rounded-lg">
+                <div
+                  className="flex justify-between items-center p-4 cursor-pointer bg-[#1495AB] text-white rounded-t-lg"
+                  onClick={() => toggleSection(index)}
+                >
+                  <h3 className="text-sm font-bold text-white">
+                    Consultation Date:{" "}
+                    {convertToIST(consultation?.bookingDetails?.createdAt)}
+                  </h3>
+                  {openSection === index ? <FaChevronUp /> : <FaChevronDown />}
+                </div>
 
-              {openSection === index && (
-                <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4 space-y-2 sm:space-y-0">
-                  {/* Consultation Formats */}
-                  <div className="flex flex-col">
-                    <span className="text-sm text-gray-500">
-                      Consultation Formats
-                    </span>
-                    <span className="text-sm text-gray-800 font-bold">
-                      {consultation?.bookingDetails?.consultation_formats ===
-                      "videoCall" ? (
-                        <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-1">
-                          <p className="text-xm">Video Call</p>
-                          <MdOutlineVideoCall className="text-xl" />
-                        </div>
-                      ) : consultation?.bookingDetails?.consultation_formats ===
-                        "chat" ? (
-                        <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-2">
-                          <p>Chat </p>
-                          <CiChat1 />
-                        </div>
-                      ) : consultation?.bookingDetails?.consultation_formats ===
-                        "phoneCall" ? (
-                        <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-2">
-                          <p>Audio Call</p>
-                          <MdSpatialAudioOff />
-                        </div>
-                      ) : (
-                        <div></div>
-                      )}
-                    </span>
-                  </div>
+                {openSection === index && (
+                  <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4 space-y-2 sm:space-y-0">
+                    {/* Consultation Formats */}
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-500">
+                        Consultation Formats
+                      </span>
+                      <span className="text-sm text-gray-800 font-bold">
+                        {consultation?.bookingDetails?.consultation_formats ===
+                        "videoCall" ? (
+                          <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-1">
+                            <p className="text-xm">Video Call</p>
+                            <MdOutlineVideoCall className="text-xl" />
+                          </div>
+                        ) : consultation?.bookingDetails
+                            ?.consultation_formats === "chat" ? (
+                          <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-2">
+                            <p>Chat</p>
+                            <CiChat1 />
+                          </div>
+                        ) : consultation?.bookingDetails
+                            ?.consultation_formats === "phoneCall" ? (
+                          <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-2">
+                            <p>Audio Call</p>
+                            <MdSpatialAudioOff />
+                          </div>
+                        ) : null}
+                      </span>
+                    </div>
 
-                  {/* Select Mode */}
-                  <div className="flex flex-col">
-                    <span className="text-sm text-gray-500">Select Mode</span>
-                    <span className="text-lg font-semibold text-gray-800">
-                      <div className="flex ">
+                    {/* Select Mode */}
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-500">Select Mode</span>
+                      <span className="text-lg font-semibold text-gray-800">
                         {consultation?.bookingDetails?.selectMode ===
                         "online" ? (
                           <div className="flex gap-2 border sm:w-[30%] w-full rounded-xl justify-center items-center p-1">
@@ -106,74 +106,81 @@ const Bookings = ({ history }) => {
                             <IoCloudOffline />
                           </div>
                         ) : null}
-                      </div>
-                    </span>
-                  </div>
+                      </span>
+                    </div>
 
-                  {/* Short Description */}
-                  <div className="flex flex-col md:col-span-2">
-                    <span className="text-sm text-gray-500">
-                      Patient Problem
-                    </span>
-                    <p className="font-medium text-gray-800 text-sm">
-                      {consultation.bookingDetails?.short_description}
-                    </p>
-                  </div>
+                    {/* Short Description */}
+                    <div className="flex flex-col md:col-span-2">
+                      <span className="text-sm text-gray-500">
+                        Patient Problem
+                      </span>
+                      <p className="font-medium text-gray-800 text-sm">
+                        {consultation?.bookingDetails?.short_description}
+                      </p>
+                    </div>
 
-                  {/* Start Time */}
-                  <div className="flex flex-col">
-                    <span className="text-sm text-gray-500">Start Time</span>
-                    <span className="text-lg font-semibold text-gray-800">
-                      {
-                        consultation?.bookingDetails?.specificSlotData
-                          ?.startTime
-                      }
-                    </span>
-                  </div>
+                    {/* Start Time */}
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-500">Start Time</span>
+                      <span className="text-lg font-semibold text-gray-800">
+                        {
+                          consultation?.bookingDetails?.specificSlotData
+                            ?.startTime
+                        }
+                      </span>
+                    </div>
 
-                  {/* End Time */}
-                  <div className="flex flex-col">
-                    <span className="text-sm text-gray-500">End Time</span>
-                    <span className="text-lg font-semibold text-gray-800">
-                      {consultation?.bookingDetails?.specificSlotData?.endTime}
-                    </span>
-                  </div>
+                    {/* End Time */}
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-500">End Time</span>
+                      <span className="text-lg font-semibold text-gray-800">
+                        {
+                          consultation?.bookingDetails?.specificSlotData
+                            ?.endTime
+                        }
+                      </span>
+                    </div>
 
-                  {/* Updated At */}
-                  <div className="flex flex-col">
-                    <span className="text-sm text-gray-500">Last Updated</span>
-                    <span className="text-sm font-semibold text-gray-800">
-                      <div className="flex sm:w-[30%] w-full rounded-xl border justify-center items-center p-1 gap-2">
-                        <span>
-                          {convertToIST(
-                            consultation?.bookingDetails?.updatedAt
-                          )}
-                        </span>
-                        <IoIosTimer />
-                      </div>
-                    </span>
-                  </div>
+                    {/* Updated At */}
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-500">
+                        Last Updated
+                      </span>
+                      <span className="text-sm font-semibold text-gray-800">
+                        <div className="flex sm:w-[30%] w-full rounded-xl border justify-center items-center p-1 gap-2">
+                          <span>
+                            {convertToIST(
+                              consultation?.bookingDetails?.updatedAt
+                            )}
+                          </span>
+                          <IoIosTimer />
+                        </div>
+                      </span>
+                    </div>
 
-                  {/* User ID */}
-                  <div className="flex flex-col">
-                    <span className="text-sm text-gray-500">Booking ID</span>
-                    <span className="text-sm font-semibold text-gray-800">
-                      {consultation?.bookingDetails?._id}
-                    </span>
-                  </div>
+                    {/* User ID */}
+                    <div className="flex flex-col">
+                      <span className="text-sm text-gray-500">Booking ID</span>
+                      <span className="text-sm font-semibold text-gray-800">
+                        {consultation?.bookingDetails?._id}
+                      </span>
+                    </div>
 
-                  {/* Make prescription */}
-                  <div className="flex flex-col sm:w-[150px] w-full ">
-                    <button className="bg-[#944120] hover:bg-[#6e341d] transition-all duration-500 ease-in-out p-2 text-white rounded-md">
-                      Make prescription
-                    </button>
+                    {/* Make Prescription */}
+                    <div className="flex flex-col sm:w-[150px] w-full">
+                      <button className="bg-[#944120] hover:bg-[#6e341d] transition-all duration-500 ease-in-out p-2 text-white rounded-md">
+                        Make Prescription
+                      </button>
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-          ))}
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="text-center text-gray-500">No Booking Till Now</div>
+      )}
     </div>
   );
 };
