861ea51d21a923061c7b01a9d1391041017063dd
unknown <gaurav.mishracse@acem.edu.in>
2024-12-09 17:58:28 +0530

    history

--- src/Pages/AppointmentBooking/ReturningPatient/ConsultwithLast/ConsultwithLastDoctor.jsx
+++ src/Pages/AppointmentBooking/ReturningPatient/ConsultwithLast/ConsultwithLastDoctor.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React , {useEffect , useState} from 'react'
 
 const ConsultwithLastDoctor = () => {
+  const [userBooking, setUserBooking] = useState([]);
+
+  // Mock tokens and IDs (Replace these with your actual logic)
+  const token = localStorage.getItem("token");
+  const id = localStorage.getItem("id");
+  const userBookings = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.assetorix.com/ah/api/v1/dc/user/history`,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+            id: id,
+          },
+        }
+      );
+      setUserBooking(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    userBookings();
+  }, []);
   return (
-    <div>ConsultwithLastDoctor</div>
+    <div>
+
+    </div>
   )
 }
 
